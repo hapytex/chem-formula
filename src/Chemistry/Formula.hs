525f9de1e5e6f8dcdcabf7d04bde3060c4461a9b
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-03-31 12:51:46 +0200

    rendering a formula

--- src/Chemistry/Formula.hs
+++ src/Chemistry/Formula.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings, TypeFamilies #-}
 
 module Chemistry.Formula where
 
@@ -10,6 +10,7 @@ import Control.Applicative(liftA2)
 import Data.HashMap.Strict(HashMap, fromListWith)
 import qualified Data.HashMap.Strict as HM
 import Data.List.NonEmpty(NonEmpty((:|)))
+import Data.Text(pack)
 
 import GHC.Exts(IsList(Item, fromList, toList))
 
@@ -19,39 +20,38 @@ import Numeric.Units.Dimensional.NonSI(dalton)
 
 data FormulaPart
     = Element Element
-    | Times Formula Int
+    | Formula :* Int
     deriving (Eq, Ord, Read, Show)
 
 data Formula
     = FormulaPart FormulaPart
-    | Combine FormulaPart Formula
+    | FormulaPart :- Formula
     deriving (Eq, Ord, Read, Show)
-
 
 formulaToParts :: Formula -> NonEmpty FormulaPart
 formulaToParts (FormulaPart p) = p :| []
-formulaToParts (Combine p f) = p :| toList f
+formulaToParts (p :- f) = p :| toList f
 
 instance IsList Formula where
     type Item Formula = FormulaPart
     fromList [] = error "A formula contains at least one element"
     fromList (x:xs) = go xs x
         where go [] = FormulaPart
-              go (x:xs) = (`Combine` go xs x)
+              go (x:xs) = (:- go xs x)
     toList (FormulaPart p) = [p]
-    toList (Combine p f) = p : toList f
+    toList (p :- f) = p : toList f
 
 instance Semigroup Formula where
-    (<>) (FormulaPart p) = Combine p
-    (<>) (Combine f p) = Combine f . (p <>)
+    (<>) (FormulaPart p) = (p :-)
+    (<>) (f :- p) = (f :-) . (p <>)
 
 _listElements :: Formula -> [(Element, Int)]
 _listElements = go 1 []
     where go n = go'
-              where go' tl (Combine p f) = go'' (go' tl f) p
+              where go' tl (p :- f) = go'' (go' tl f) p
                     go' tl (FormulaPart p) = go'' tl p
                     go'' tl (Element e) = (e, n) : tl
-                    go'' tl (Times f n') = go (n*n') tl f
+                    go'' tl (f :* n') = go (n*n') tl f
                     
 
 _listElements' :: Formula -> HashMap Element Int
@@ -61,9 +61,17 @@ molecularMass :: Floating a => Formula -> Maybe (Quantity DMass a)
 molecularMass = foldr (liftA2 (D.+) . (\(e, n) -> ((fromIntegral n *~ one) D.*) <$> weight e)) (Just (0 *~ dalton)) . _listElements
 
 toMolecular :: Formula -> Formula
-toMolecular = fromList . map (uncurry (Times . FormulaPart . Element)) . HM.toList . _listElements'
+toMolecular = fromList . map (uncurry ((:*) . FormulaPart . Element)) . HM.toList . _listElements'
 -- toEmpirical :: Formula -> Formula
 
+instance FormulaElement FormulaPart where
+    toFormula (Element e) = toFormula e
+    toFormula ((FormulaPart (Element e)) :* n) = toFormula e <> pack (show n)
+    toFormula (f :* n) = "(" <> toFormula f <> ")" <> pack (show n)
+    weight (f :* n) = ((fromIntegral n *~ one) D.*) <$> weight f
+    weight (Element e) = weight e
+
 instance FormulaElement Formula where
-    toFormula = undefined
+    toFormula (FormulaPart p) = toFormula p
+    toFormula (p :- f) = toFormula p <> toFormula f
     weight = molecularMass
