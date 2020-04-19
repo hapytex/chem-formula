b733dad2519fd9b4042ecbdbd8a879c79f67b937
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-04-19 12:04:41 +0200

    lift Formula

--- src/Chemistry/Formula.hs
+++ src/Chemistry/Formula.hs
@@ -3,11 +3,11 @@
 module Chemistry.Formula where
 
 import Chemistry.Core(FormulaElement(toFormula, weight))
-import Chemistry.Element(Element)
 
 import Control.Applicative(liftA2)
 
 import Data.Char.Small(asSub)
+import Data.Hashable(Hashable)
 import Data.HashMap.Strict(HashMap, fromListWith)
 import qualified Data.HashMap.Strict as HM
 import Data.List.NonEmpty(NonEmpty((:|)))
@@ -20,30 +20,30 @@ import qualified Numeric.Units.Dimensional as D
 import Numeric.Units.Dimensional.NonSI(dalton)
 
 import Test.QuickCheck(Gen, oneof)
-import Test.QuickCheck.Arbitrary(Arbitrary(arbitrary))
+import Test.QuickCheck.Arbitrary(Arbitrary(arbitrary), Arbitrary1(liftArbitrary), arbitrary1)
 
 infix 8 :*
 infixr 7 :-
 
-data FormulaPart
-    = Element Element
-    | Formula :* Int
+data FormulaPart a
+    = Element a
+    | (Formula a) :* Int
     deriving (Eq, Ord, Read, Show)
 
-data Formula
-    = FormulaPart FormulaPart
-    | FormulaPart :- Formula
+data Formula a
+    = FormulaPart (FormulaPart a)
+    | (FormulaPart a) :- (Formula a)
     deriving (Eq, Ord, Read, Show)
 
-(.*) :: FormulaPart -> Int -> FormulaPart
+(.*) :: FormulaPart a -> Int -> FormulaPart a
 (.*) = (:*) . FormulaPart
 
-formulaToParts :: Formula -> NonEmpty FormulaPart
+formulaToParts :: Formula a -> NonEmpty (FormulaPart a)
 formulaToParts (FormulaPart p) = p :| []
 formulaToParts (p :- f) = p :| toList f
 
-instance IsList Formula where
-    type Item Formula = FormulaPart
+instance IsList (Formula a) where
+    type Item (Formula a) = (FormulaPart a)
     fromList [] = error "A formula contains at least one element"
     fromList (fp:fps) = go fps fp
         where go [] = FormulaPart
@@ -51,11 +51,11 @@ instance IsList Formula where
     toList (FormulaPart p) = [p]
     toList (p :- f) = p : toList f
 
-instance Semigroup Formula where
+instance Semigroup (Formula a) where
     (<>) (FormulaPart p) = (p :-)
     (<>) (f :- p) = (f :-) . (p <>)
 
-_listElements :: Formula -> [(Element, Int)]
+_listElements :: Formula a -> [(a, Int)]
 _listElements = go 1 []
     where go n = go'
               where go' tl (p :- f) = go'' (go' tl f) p
@@ -64,17 +64,17 @@ _listElements = go 1 []
                     go'' tl (f :* n') = go (n*n') tl f
                     
 
-_listElements' :: Formula -> HashMap Element Int
+_listElements' :: (Eq a, Hashable a) => Formula a -> HashMap a Int
 _listElements' = fromListWith (+) . _listElements
 
-molecularMass :: Floating a => Formula -> Maybe (Quantity DMass a)
+molecularMass :: (Floating a, FormulaElement b) => Formula b -> Maybe (Quantity DMass a)
 molecularMass = foldr (liftA2 (D.+) . (\(e, n) -> ((fromIntegral n *~ one) D.*) <$> weight e)) (Just (0 *~ dalton)) . _listElements
 
-toMolecular :: Formula -> Formula
+toMolecular :: (Eq a, Hashable a) => Formula a -> Formula a
 toMolecular = fromList . map (uncurry ((:*) . FormulaPart . Element)) . HM.toList . _listElements'
 -- toEmpirical :: Formula -> Formula
 
-instance FormulaElement FormulaPart where
+instance FormulaElement a => FormulaElement (FormulaPart a) where
     toFormula (Element e) = toFormula e
     toFormula (f :* n) = go f <> asSub n
         where go (FormulaPart (Element e)) = toFormula e
@@ -82,7 +82,7 @@ instance FormulaElement FormulaPart where
     weight (f :* n) = ((fromIntegral n *~ one) D.*) <$> weight f
     weight (Element e) = weight e
 
-instance FormulaElement Formula where
+instance FormulaElement a => FormulaElement (Formula a) where
     toFormula (FormulaPart p) = toFormula p
     toFormula (p :- f) = toFormula p <> toFormula f
     weight = molecularMass
@@ -90,8 +90,16 @@ instance FormulaElement Formula where
 _positiveGen :: Gen Int
 _positiveGen = (1+) . abs <$> arbitrary
 
-instance Arbitrary FormulaPart where
-    arbitrary = oneof [ Element <$> arbitrary, (:*) <$> arbitrary <*> _positiveGen ]
+instance Arbitrary a => Arbitrary (FormulaPart a) where
+    arbitrary = arbitrary1 -- oneof [ Element <$> arbitrary, (:*) <$> arbitrary <*> _positiveGen ]
 
-instance Arbitrary Formula where
-    arbitrary = oneof [ (:-) <$> arbitrary <*> arbitrary, FormulaPart <$> arbitrary ]
+instance Arbitrary a => Arbitrary (Formula a) where
+    arbitrary = arbitrary1 -- oneof [ (:-) <$> arbitrary <*> arbitrary, FormulaPart <$> arbitrary ]
+
+instance Arbitrary1 FormulaPart where
+    liftArbitrary arb = oneof [Element <$> arb, (:*) <$> liftArbitrary arb <*> _positiveGen]
+
+instance Arbitrary1 Formula where
+    liftArbitrary arb = go
+        where go = oneof [(:-) <$> arb' <*> go, FormulaPart <$> arb']
+              arb' = liftArbitrary arb
